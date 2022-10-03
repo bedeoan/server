e26e73bd3deedf969607341ad5486799247a921e
bede <bedeoan.raul@gmail.com>
2022-10-03 15:25:53 +0300

    hf

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 let app = express();
-app.use(serveStatic(__dirname + '/client/dist'));
+app.use(serveStatic(__dirname + '/dist'));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
